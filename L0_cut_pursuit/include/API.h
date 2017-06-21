9ec0c2a9a1cb90bd169eaf4f852b391cca455c4d
Loic Landrieu <landrieu.loic@gmail.com>
2017-06-21 16:37:02 +0200

    Add files via upload

--- L0_cut_pursuit/include/API.h
+++ L0_cut_pursuit/include/API.h
@@ -4,6 +4,71 @@
 #include "CutPursuit_Linear.h"
 #include "CutPursuit_KL.h"
 
+//**********************************************************************************
+//*******************************L0-CUT PURSUIT*************************************
+//**********************************************************************************
+//Greedy graph cut based algorithm to solve the generalized minimal
+//partition problem
+//
+//Cut Pursuit: fast algorithms to learn piecewise constant functions on
+//general weighted graphs, Loic Landrieu and Guillaume Obozinski,2016.
+//
+//Produce a piecewise constant approximation of signal $y$ structured
+//by the graph G=(V,e,mu,w) with mu the node weight and w the edgeweight:
+//argmin \sum_{i \IN V}{mu_i * phi(x_I, y_I)}
+//+ \sum_{(i,j) \IN E}{w_{i,j} 1(x_I != x_J)}
+//
+//phi(X,Y) the fidelity function (3 are implemented)
+//(x != y) the funciton equal to 1 if x!=y and 0 else
+//
+// LOIC LANDRIEU 2017
+//
+//=======================SYNTAX===================================================
+//C style inputs
+//void cut_pursuit(const int nNodes, const int nEdges, const int nObs
+//          ,const T * observation, const int * Eu, const int * Ev
+//          ,const T * edgeWeight, const T * nodeWeight
+//          ,T * solution,  const T lambda, const T mode, const T speed
+//          , const float verbose)
+//C++ style input
+//void cut_pursuit(const int nNodes, const int nEdges, const int nObs
+//          , std::vector< std::vector<T> > & observation
+//          , const std::vector<int> & Eu, const std::vector<int> & Ev
+//          ,const std::vector<T> & edgeWeight, const std::vector<T> & nodeWeight
+//          ,std::vector< std::vector<T> > & solution,  const T lambda, const T mode, const T speed
+ //         , const float verbose)
+//-----INPUT-----
+// 1x1 int nNodes = number of nodes
+// 1x1 int nEdges = number of edges
+// 1x1 int nObs   = dimension of data on each node
+// NxD float observation : the observed signal
+// Ex1 int Eu, Ev: the origin and destination of each node
+// Ex1 float  edgeWeight: the edge weight
+// Nx1 float  nodeWeight: the node weight
+// 1x1 float lambda : the regularization strength
+// 1x1 float mode : the fidelity function
+//      0 : linear (for simplex bound data)
+//      1 : quadratic (default)
+//   0<a<1: KL with a smoothing (for simplex bound data)
+// 1x1 float speed : parametrization impacting performance
+//      0 : slow but precise
+//      1 : recommended (default)
+//      2 : fast but approximated (no backward step)
+//      3 : ludicrous - for prototyping (no backward step)
+// 1x1 bool verose : verbosity
+//      0 : silent
+//      1 : recommended (default)
+//      2 : chatty
+//-----OUTPUT-----
+// Nx1 float  solution: piecewise constant approximation
+// Nx1 int inComponent: for each node, in which component it belongs
+// n_node_redx1 cell components : for each component, list of the nodes
+// 1x1 n_node_red : number of components
+// 1x1 int n_edges_red : number of edges in reduced graph
+// n_edges_redx1 int Eu_red, Ev_red : source and target of reduced edges
+// n_edges_redx1 float edgeWeight_red: weights of reduced edges
+// n_node_redx1  float nodeWeight_red: weights of reduced nodes
+
 namespace CP {
 
 //===========================================================================
@@ -332,8 +397,7 @@ void set_speed(CutPursuit<T> * cp, const T speed, const float verbose)
         cp->parameter.backward_step = true;
         cp->parameter.stopping_ratio = 0;
     }
-/*
-    else
+    else if (speed == 1)
     {
         if (verbose > 0)
         {
