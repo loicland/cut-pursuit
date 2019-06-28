8d541901deb42250450c0270853888b6da310c25
Loic Landrieu <landrieu.loic@gmail.com>
2019-06-28 16:37:42 +0200

    added the modified python wrapper

--- src/cutpursuit.cpp
+++ src/cutpursuit.cpp
@@ -7,6 +7,7 @@
 #include "boost/tuple/tuple.hpp"
 #include "boost/python/object.hpp"
 #include <../include/API.h>
+//#include <../include/connected_components.h>
 
 namespace bpn = boost::python::numpy;
 namespace bp =  boost::python;
@@ -15,27 +16,28 @@ typedef boost::tuple< std::vector< std::vector<uint32_t> >, std::vector<uint32_t
 
 struct VecToArray
 {//converts a vector<uint32_t> to a numpy array
-    static PyObject * convert(const std::vector<uint32_t> & vec) {
-    npy_intp dims = vec.size();
-    PyObject * obj = PyArray_SimpleNew(1, &dims, NPY_UINT32);
-    void * arr_data = PyArray_DATA((PyArrayObject*)obj);
-    memcpy(arr_data, &vec[0], dims * sizeof(uint32_t));
-    return obj;
+    static PyObject * convert(const std::vector<uint32_t> & vec)
+    {
+        npy_intp dims = vec.size();
+        PyObject * obj = PyArray_SimpleNew(1, &dims, NPY_UINT32);
+        void * arr_data = PyArray_DATA((PyArrayObject*)obj);
+        memcpy(arr_data, &vec[0], dims * sizeof(uint32_t));
+        return obj;
     }
 };
 
-struct FloatVecToArray
+struct VecToArray_float
 {//converts a vector<uint32_t> to a numpy array
-    static PyObject * convert(const std::vector<float> & vec, uint32_t n_ver, uint32_t n_obs) {
-    npy_intp dims[2];
-    dims[0] = (npy_intp)n_ver;
-    dims[1] = (npy_intp)n_obs;
-    PyObject * obj = PyArray_SimpleNew(2, dims, NPY_FLOAT32);
-    void * arr_data = PyArray_DATA((PyArrayObject*)obj);
-    memcpy(arr_data, &vec[0], n_ver * n_obs * sizeof(float));
-    return obj;
+    static PyObject * convert(const std::vector<float> & vec)
+    {
+        npy_intp dims = vec.size();
+        PyObject * obj = PyArray_SimpleNew(1, &dims, NPY_FLOAT32);
+        void * arr_data = PyArray_DATA((PyArrayObject*)obj);
+        memcpy(arr_data, &vec[0], dims * sizeof(float));
+        return obj;
     }
 };
+
 
 template<class T>
 struct VecvecToList
@@ -72,8 +74,11 @@ struct to_py_tuple
     }
 };
 
-PyObject * cutpursuit_seg(const bpn::ndarray & obs, const bpn::ndarray & source, const bpn::ndarray & target,const bpn::ndarray & edge_weight,  float lambda)
+PyObject * cutpursuit(const bpn::ndarray & obs, const bpn::ndarray & source, const bpn::ndarray & target,const bpn::ndarray & edge_weight,
+                      float lambda, const int cutoff, const int spatial)
 {//read data and run the L0-cut pursuit partition algorithm
+    srand(0);
+
     const uint32_t n_ver = bp::len(obs);
     const uint32_t n_edg = bp::len(source);
     const uint32_t n_obs = bp::len(obs[0]);
@@ -81,19 +86,29 @@ PyObject * cutpursuit_seg(const bpn::ndarray & obs, const bpn::ndarray & source,
     const uint32_t * source_data = reinterpret_cast<uint32_t*>(source.get_data());
     const uint32_t * target_data = reinterpret_cast<uint32_t*>(target.get_data());
     const float * edge_weight_data = reinterpret_cast<float*>(edge_weight.get_data());
-//    float solution [n_ver * n_obs];
-    std::vector<float> solution(n_ver * n_obs, 0.0f);
+    std::vector<float> solution(n_ver *n_obs);
+    //float solution [n_ver * n_obs];
     std::vector<float> node_weight(n_ver, 1.0f);
     std::vector<uint32_t> in_component(n_ver,0);
     std::vector< std::vector<uint32_t> > components(1,std::vector<uint32_t>(1,0.f));
-    CP::cut_pursuit<float>(n_ver, n_edg, n_obs, obs_data, source_data, target_data, edge_weight_data, &node_weight[0]
-             , solution.data(), in_component, components, lambda, 1.f, 2.f, 2.f);
-//    delete[]solution
+    if (spatial == 0)
+    {
+        CP::cut_pursuit<float>(n_ver, n_edg, n_obs, obs_data, source_data, target_data, edge_weight_data, &node_weight[0]
+                 , solution.data(), in_component, components, lambda, (uint32_t)cutoff,  2.f, 4.f, 0.f);
+    }
+    else
+    {
+        CP::cut_pursuit<float>(n_ver, n_edg, n_obs, obs_data, source_data, target_data, edge_weight_data, &node_weight[0]
+                 , solution.data(), in_component, components, lambda, (uint32_t)cutoff,  2.f, 4.f, 0.f);
+    }
     return to_py_tuple::convert(Custom_tuple(components, in_component));
 }
 
-PyObject * cutpursuit_reg(const bpn::ndarray & obs, const bpn::ndarray & source, const bpn::ndarray & target,const bpn::ndarray & edge_weight,  float lambda)
+PyObject * cutpursuit2(const bpn::ndarray & obs, const bpn::ndarray & source, const bpn::ndarray & target,const bpn::ndarray & edge_weight,
+                       const bpn::ndarray & node_weight, float lambda)
 {//read data and run the L0-cut pursuit partition algorithm
+    srand(0);
+std::cout << std::endl;
     const uint32_t n_ver = bp::len(obs);
     const uint32_t n_edg = bp::len(source);
     const uint32_t n_obs = bp::len(obs[0]);
@@ -101,24 +116,26 @@ PyObject * cutpursuit_reg(const bpn::ndarray & obs, const bpn::ndarray & source,
     const uint32_t * source_data = reinterpret_cast<uint32_t*>(source.get_data());
     const uint32_t * target_data = reinterpret_cast<uint32_t*>(target.get_data());
     const float * edge_weight_data = reinterpret_cast<float*>(edge_weight.get_data());
-//    float solution [n_ver * n_obs];
-    std::cout << n_ver << " " << n_obs << std::endl;
-    std::vector<float> solution(n_ver * n_obs, 0.0f);
-    std::vector<float> node_weight(n_ver, 1.0f);
+    const float * node_weight_data = reinterpret_cast<float*>(node_weight.get_data());
+    std::vector<float> solution(n_ver *n_obs);
+    //float solution [n_ver * n_obs];
+    //std::vector<float> node_weight(n_ver, 1.0f);
+    std::vector<uint32_t> in_component(n_ver,0);
     std::vector< std::vector<uint32_t> > components(1,std::vector<uint32_t>(1,0.f));
-    CP::cut_pursuit<float>(n_ver, n_edg, n_obs, obs_data, source_data, target_data, edge_weight_data, &node_weight[0]
-             , solution.data(), lambda, 1.f, 2.f, 2.f);
-//    delete[]solution
-    return FloatVecToArray::convert(solution, n_ver, n_obs);
+    CP::cut_pursuit<float>(n_ver, n_edg, n_obs, obs_data, source_data, target_data, edge_weight_data, node_weight_data
+                 , solution.data(), in_component, components, lambda, (uint32_t)0,  2.f, 4.f, 0.f);
+    return to_py_tuple::convert(Custom_tuple(components, in_component));
 }
 
 BOOST_PYTHON_MODULE(libcp)
 {
     _import_array();
     Py_Initialize();
-    bpn::initialize();
+    //bpn::initialize();
     bp::to_python_converter< Custom_tuple, to_py_tuple>();
-    def("cutpursuit", cutpursuit_seg);
-    def("cutpursuit_reg", cutpursuit_reg);
+    //def("connected_comp", connected_comp);
+    def("cutpursuit", cutpursuit);
+    bp::def("cutpursuit", cutpursuit, (bp::args("cutoff")=0, bp::args("spatial")=0));
+    bp::def("cutpursuit2", cutpursuit2);
 }
 
