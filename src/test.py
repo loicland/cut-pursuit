ba155e45754057cb2b150f439f2379298b52fa69
Loic Landrieu <landrieu.loic@gmail.com>
2019-11-15 13:53:30 +0100

    added example of hierarchical partition to test.py

--- src/test.py
+++ src/test.py
@@ -7,4 +7,15 @@ source = np.array([0, 1], dtype = 'uint32')
 target = np.array([1, 2], dtype = 'uint32')
 edge_weight = np.array([[1., 1.]], dtype = 'float32')
 [comp, in_com] = libcp.cutpursuit(obs, source, target, edge_weight, 1)
-# sol = libcp.cutpursuit_reg(obs, source, target, edge_weight, 1)
+
+#hierarchical partition
+
+obs = np.array([[0],[1],[5],[6],[20]], dtype='float32')
+source = np.array([0,0,0,0,1,1,1,2,2,3], dtype='uint32')
+target = np.array([1,2,3,4,2,3,4,3,4,4], dtype='uint32')
+edge_weight = np.array([1,1,1,1,1,1,1,1,1,1], dtype='float32')
+reg_str = np.array([1,5], dtype='float32')
+cut_off = np.array([1,1], dtype='uint32')
+
+hier = libcp.cutpursuit_hierarchy(x, Eu, Ev,  Ew ,rs, co, 0, 0.7)
+
